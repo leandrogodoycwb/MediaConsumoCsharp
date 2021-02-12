fab8e0e3807fea45be65f5cfe12a14689e96a9b5
leandrogodoycwb <leandrolenon@gmail.com>
2021-02-12 02:46:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double consumo;
+
+            Console.WriteLine("Qual distância você vai percorrer?");
+            distancia = int.Parse(Console.ReadLine());
+
+            consumo = distancia/12.00;
+
+            Console.WriteLine(string.Format("{0:0.000} litros de gasolina", consumo));
+        }
+    }
+}
